86c8c26b55ff7a33034e1c1e82cd153d463be4e5
Maks <begimkulov.2909@gmail.com>
2019-12-24 18:39:52 +0600

    initial

---
+++ js/main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function(){
+
+    particlesJS.load('particles-js', './js/assets/particles/config.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+});
